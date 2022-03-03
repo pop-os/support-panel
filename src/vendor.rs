0d962e41a90dd2b4a8256898b7b5df8d7a666853
Michael Aaron Murphy <mmstick@pm.me>
2022-03-03 20:01:40 +0100

    refactor: `Vendor::guess_from` -> `Vendor::guess`

--- src/vendor.rs
+++ src/vendor.rs
@@ -1,15 +1,21 @@
 // Copyright 2022 System76 <info@system76.com>
 // SPDX-License-Identifier: MPL-2.0
 
+use std::fs::read_to_string;
+
 #[derive(Copy, Clone, Debug)]
 pub enum Vendor {
     System76,
 }
 
 impl Vendor {
-    pub fn guess_from(sys_vendor: &str) -> Option<Self> {
-        if "System76" == sys_vendor {
-            return Some(Vendor::System76);
+    pub fn guess() -> Option<Self> {
+        if let Ok(sys_vendor) = read_to_string("/sys/devices/virtual/dmi/id/sys_vendor") {
+            #[allow(clippy::single_match)]
+            match sys_vendor.trim() {
+                "System76" => return Some(Vendor::System76),
+                _ => (),
+            }
         }
 
         None
